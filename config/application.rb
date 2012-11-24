0f3080117e403c0020f9bf7751c76c09a5373c3b
Markus Nordin <markus.nordin@gmail.com>
2012-11-24 21:40:42 +0100

    Facebook yaml config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module Facepalm
   class Application < Rails::Application
+
+    FACEBOOK_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/facebook.yml")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
