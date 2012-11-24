6dea52e4129347c002113ff28c20b1612cc1e35d
Tim Romberg <tim.jakobsson@gmail.com>
2012-11-24 22:44:11 +0100

    use env variables instead of yaml config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['key'], FACEBOOK_CONFIG['secret'], :scope => 'email', :callback_path => ''
+  provider :facebook, ENV['facebook_key'], ENV['facebook_secret'], :scope => 'email', :callback_path => ''
 end
