1479fd6a3aebdcba989318427d72d821dad76bea
Tim Romberg <tim.jakobsson@gmail.com>
2012-11-24 21:14:15 +0100

    initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facepalm::Application.config.secret_token = 'b1604f85f98ad8197013484c9fb2c3885a790533f786adbf09e4f9e7e027024ba5605fcb85d2bd0acc5964a3b6683a72ad143eb531e2d2cd71b748a61af7e8b4'
