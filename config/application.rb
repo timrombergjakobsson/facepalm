9f734ff528552b686e459b4c2a4da53d0e8ef84d
Tim Romberg <tim.jakobsson@gmail.com>
2012-12-30 21:32:30 +0100

    added inuit.css

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,7 @@ module Facepalm
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #config.sass.load_paths << File.expand_path('../../vendor/assets/css/')
   end
 end
